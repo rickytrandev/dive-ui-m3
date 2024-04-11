9f66b7c2a7baf052e034fba35a7ddc9a7a84bdd9
Ricky Tran <therichardtran@gmail.com>
2024-04-11 15:24:58 -0600

    Style: move search button inside input field and aligned header elements

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import "./Search.css"
+import "./Search.css";
 import { useState } from "react";
 import { HeartBtn } from "../HeartBtn/HeartBtn";
 import { HomeBtn } from "../HomeBtn/HomeBtn";
@@ -6,40 +6,62 @@ import { Link, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export function Search({ handleSearch }) {
-  const [query, setQuery] = useState('');
-  const navigate = useNavigate()
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
   function handleInputChange(event) {
-    setQuery(event.target.value)
+    setQuery(event.target.value);
   }
 
   function handleKeyPress(event) {
     if (event.key === "Enter") {
-      handleSearch(query)
+      handleSearch(query);
     }
   }
 
   return (
-    <div className="search-bar" >
-      <input 
-        type="text" 
-        placeholder="Search..."
-        value={query}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyPress}
-        onClick={() => navigate('/main')}
-      />
-        <button onClick={() => handleSearch(query)} className="search-btn" >
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-            <path fill="#D46161" d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-          </svg>
-        </button>
-      <HeartBtn />
-      <HomeBtn />
-    </div>
-  )
+    <>
+      <div className="search-container">
+        <div className="search-bar">
+          <input
+            className=""
+            type="text"
+            placeholder="Search..."
+            value={query}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+            onClick={() => navigate("/main")}
+          />
+          <button onClick={() => handleSearch(query)} className="search-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill="#D46161"
+                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+        <div className="icon-container">
+          <Link to="/main">
+            <HomeBtn />
+          </Link>
+          <Link to="/favorites">
+            <HeartBtn />
+          </Link>
+        </div>
+       
+    </>
+  );
 }
 
 Search.propTypes = {
-  handleSearch: PropTypes.func
-}
+  handleSearch: PropTypes.func,
+};
