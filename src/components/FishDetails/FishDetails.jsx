c7921c4d8249ca173471b8e8e1cd29c8b891eb08
Ricky Tran <therichardtran@gmail.com>
2024-05-16 14:52:37 -0600

    fix: remove transform: scale for mobile view and fix padding for menu

--- src/components/FishDetails/FishDetails.jsx
+++ src/components/FishDetails/FishDetails.jsx
@@ -51,7 +51,7 @@ export function FishDetails({ fish, addFavorite, isMenuOpen }) {
           </article>
           <div className="buttons-container">
             <button onClick={() => addFavorite(Number(id))} className="fav-btn">
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 13">
                 <path fill={isFavorite ? "#D46161" : ""} fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
               </svg>
             </button>
